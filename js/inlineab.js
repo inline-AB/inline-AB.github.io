d9b8c6d4e4dc5e0c922dfebbfaacbfc03135c670
Peter Hayes <peter.k.hayes@gmail.com>
2013-12-02 14:22:51 -0800

    removed duplicate google analytics info

--- js/inlineab.js
+++ js/inlineab.js
@@ -8,17 +8,6 @@
 // to Google Universal Analytics for analysis.
 //
 // The file is inserted directly after the opening <body> tag in the page's HTML.
-//
-
-// Standard Google Universal Analytics setup code.
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// Set second argument with your GA Tracking ID.
-// This can be found under Admin > Property > Property Settings > Tracking ID
-ga('create', 'UA-45967923-1', 'auto');
 
 (function(window) {
 
